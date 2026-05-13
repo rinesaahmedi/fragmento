8c4352e7e70438df2d1d6dba4a445e9ed586d38e
elsaferati <142007686+elsaferati@users.noreply.github.com>
2026-05-13 08:22:42 +0200

    fixed a bug

--- frontend/components/fragmento-entry-flow.js
+++ frontend/components/fragmento-entry-flow.js
@@ -348,12 +348,6 @@ export default function FragmentoEntryFlow({ initialLanguage = "de" }) {
 
   return (
     <main style={pageStyle}>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700;800&display=swap"
-        rel="stylesheet"
-      />
       <style>{responsivePanelMedia}</style>
       <div style={centerWrapStyle}>
         <section style={panelStyle}>
